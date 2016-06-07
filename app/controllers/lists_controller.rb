8315e786ef2065b6d2d600f28fc3252cd86912a9
Maureen <mahceridwen@gmail.com>
2016-06-06 21:40:50 -0300

    Added list.

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,70 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  # GET /lists
+  # GET /lists.json
+  def index
+    @lists = List.all
+  end
+
+  # GET /lists/1
+  # GET /lists/1.json
+  def show
+  end
+
+  # GET /lists/new
+  def new
+    @list = List.new
+  end
+
+  # GET /lists/1/edit
+  def edit
+  end
+
+  # POST /lists
+  # POST /lists.json
+  def create
+    @list = current_user.lists.new(list_params)
+
+    respond_to do |format|
+      if @list.save
+        format.js { flash[:notice]='List was successfully created.' }
+      else
+        format.js { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /lists/1
+  # PATCH/PUT /lists/1.json
+  def update
+    respond_to do |format|
+      if @list.update(list_params)
+        format.js { flash[:notice]='List was successfully updated.'}
+      else
+        format.js { render :edit }
+      end
+    end
+  end
+
+  # DELETE /lists/1
+  # DELETE /lists/1.json
+  def destroy
+    @list.destroy
+    respond_to do |format|
+      format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_list
+      @list = List.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def list_params
+      params.require(:list).permit(:name, :status, :user_id)
+    end
+end
---
+++ spec/factories/lists.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :list do
+    name "MyString"
+    status false
+    user_id 1
+  end
+end
